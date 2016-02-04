99ddf456c1cf83cd3129cfb3e3ed81ee3af2fa5b
David Liu <zliu3301@gmail.com>
2016-02-03 20:09:52 -0500

    api working

--- execute.py
+++ execute.py
@@ -2,46 +2,93 @@ from lxml import html
 import requests
 import challonge
 
-user = raw_input("Enter your challonge username: ");
-user = "uiblis";
-api_key = raw_input("Enter your api_key: ");
-api_key = "GqEWndsdFiH7JhkIc3l6Ezf8eg3VyxHkACjQN5Pa"
+class Player:
+    def __init__(self):
+        self.wins = 0;
+        self.losses = 0;
+        self.elo = 0;
+        self.matches = [];
+            
+    def addMatch(match):
+        matches.append[match];
 
-challonge.set_credentials(user, api_key)
-
-print("Begin");
-
-while True:
-    
-    url = raw_input("Enter a challonge url (eg. challonge.com/ladulite1): ");
-
-    if ("challonge" not in url):
-        print ("please enter a valid url");
-
-    elif (url[:9] != "challonge"):
-        x, y, z = url.split(".");
-        a, b = z.split("/");
-        url = x + "-" + b;
-        break;
+class Match:
+    def __init__(self):
+        self.winner = ""
+        self.loser = ""
+        self.score = ""
         
-    else:
-        a, b = url.split("/")
-        url = b;
-        print url;
-        break;
+    def setWinner(winner):
+        self.winner = winner;
+        
+    def setLoser(loser):
+        self.loser = loser;
+
+    def setScore(score):
+        self.score = score;
+        
+def main():
+    user = raw_input("Enter your challonge username: ");
+    user = "uiblis";
+    api_key = raw_input("Enter your api_key: ");
+    api_key = "GqEWndsdFiH7JhkIc3l6Ezf8eg3VyxHkACjQN5Pa"
+
+    challonge.set_credentials(user, api_key)
+
+    print("Begin");
+
+    listPlayers = [];
+
     
-tournament = challonge.tournaments.show(url)
+        
+    while True:
+    
+        url = raw_input("Enter a challonge url (eg. challonge.com/ladulite1): ");
 
-print ""
-print "Tournament: " + tournament["name"];
-print "Date: ",
-print tournament["started-at"];
-print ""
-decision = raw_input("Compile data from this tournament? (y/n) ");
+        if (url[:4] == "www."):
+            url = url[4:];
+        
+        if ("challonge.com" not in url):
+                print ("please enter a valid url");
 
-if (decision == "n"):
-    exit
+        elif (url[:13] != "challonge.com"):
+            x, y, z = url.split(".");
+            a, b = z.split("/");
+            url = x + "-" + b;
+            break;
+    
+        else:
+            a, b = url.split("/")
+            url = b;
+            break;
+    
+        tournament = challonge.tournaments.show(url)
 
+        print ""
+        print "Tournament: " + tournament["name"];
+        print "Date: ",
+        print tournament["started-at"];
+        print ""
+        decision = raw_input("Compile data from this tournament? (y/n) ");
+
+        while True:
+            if (decision == "n"):
+                exit;
+
+            elif (decision == "y"):
+                participants = compileData(url);
+                print participants;
+                break;
+    
+            else:
+                print("Please enter 'y' or 'n'. ");
+
+def compileData(url):
+    participants = challonge.participants.index(url);
+    return participants;
+
+if __name__ == "__main__":
+    main()
 
     
     
