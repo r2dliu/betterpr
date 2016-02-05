470260ee7b9fce7ed79219a096c9f2e0929fbed4
David Liu <zliu3301@gmail.com>
2016-02-05 14:27:46 -0500

    trueskill working

--- execute.py
+++ execute.py
@@ -2,8 +2,10 @@ from lxml import html
 import requests
 import challonge
 import json
+from trueskill import Rating, quality_1vs1, rate_1vs1
 
 listPlayers = [];
+ranking = [];
 
 class Player:
     def __init__(self, name):
@@ -12,9 +14,22 @@ class Player:
         self.losses = 0;
         self.elo = 0;
         self.matches = [];
+        self.rating = Rating();
+        self.rank = -1;
             
     def addMatch(match):
         matches.append[match];
+
+    def updateRating(self, rating):
+        self.rating = rating;
+
+    def showInfo(self):
+        print (self.name),
+        print ("%dW - %dL" %(self.wins, self.losses));
+        print ("Rating:"),
+        print self.rating,
+        print ("Rank: %d" %(self.rank));
+        
 
 class Match:
     def __init__(self):
@@ -94,6 +109,14 @@ def main():
             compileMatches(url, tourneyMatches, tourneyPlayers, tournament);
             for match in tourneyMatches:
                 match.showInfo();
+            
+            listPlayers.sort(key = lambda x: x.rating.mu, reverse = True)
+            index = 1;
+            for player in listPlayers:
+                player.rank = index;
+                index +=1;
+                player.showInfo();
+                
             break;
             
         else:
@@ -112,15 +135,31 @@ def compileMatches(url, tourneyMatches, tourneyPlayers, tournament):
      matches = challonge.matches.index(url);
      counter = 0;
      for match in matches:
+         #make match object, add data
          tourneyMatches.append(Match());
          tourneyMatches[counter].setTourney(tournament["name"]);
          for player in tourneyPlayers:
              if match["winner-id"] == player[0]:
                  tourneyMatches[counter].setWinner(player[1]);
+                 for person in listPlayers:
+                     if person.name == player[1]:
+                         winner = person;
              if match["loser-id"] == player[0]:
                  tourneyMatches[counter].setLoser(player[1]);
+                 for person in listPlayers:
+                     if person.name == player[1]:
+                         loser = person;
+        
          tourneyMatches[counter].setScore(match["scores-csv"]);
          tourneyMatches[counter].setRound(match["round"]);
+
+         #update player
+         new_r1, new_r2 = rate_1vs1(winner.rating, loser.rating)
+         winner.updateRating(new_r1);
+         loser.updateRating(new_r2);
+         winner.wins = winner.wins + 1;
+         loser.losses = loser.losses + 1;
+             
          counter = counter + 1;
          
 if __name__ == "__main__":
